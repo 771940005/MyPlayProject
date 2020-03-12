55544a3bc383a585b81220ca0bafd116bf241c8a
hanjianyong <hanjianyong@qq.com>
2020-03-12 17:15:32 +0800

    设置菜单点击事件处理

--- app/src/main/java/com/smt/myplaytest/util/ToolBarManager.kt
+++ app/src/main/java/com/smt/myplaytest/util/ToolBarManager.kt
@@ -1,7 +1,11 @@
 package com.smt.myplaytest.util
 
+import android.content.Intent
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.smt.myplaytest.R
+import com.smt.myplaytest.ui.activity.SettingActivity
 
 
 /**
@@ -17,5 +21,25 @@ interface ToolBarManager {
     fun initMainToolBar() {
         toobar.title = "我的影音"
         toobar.inflateMenu(R.menu.main)
+        // kotlin 和 Java 调用特性
+        // 如果Java接口中只有一个未实现的方法，可以省略接口对象，直接用{}表示未实现的方法
+        toobar.setOnMenuItemClickListener {
+            toobar.context.startActivity(Intent(toobar.context, SettingActivity::class.java))
+            true
+        }
+//        toobar.setOnMenuItemClickListener { item ->
+//            when (item?.itemId) {
+//                // 跳转到设置界面
+//                R.id.setting -> toobar.context.startActivity(
+//                    Intent(
+//                        toobar.context,
+//                        SettingActivity::class.java
+//                    )
+//                )
+//
+//            }
+//
+//            true
+//        }
     }
 }
